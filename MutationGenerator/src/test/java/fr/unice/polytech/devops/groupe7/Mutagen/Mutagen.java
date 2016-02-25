dbe8b22b02e822ce3156586473ccd46f5c862636
Nathael Nogues <nogues.nathael@etu.unice.fr>
2016-02-25 17:21:50 +0100

    Revert "[import] Spoon"
    This reverts commit a7af902754e2608f143dadd04a392b668cd376b7.

---
+++ MutationGenerator/src/test/java/fr/unice/polytech/devops/groupe7/Mutagen/Mutagen.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.devops.groupe7.Mutagen;
+
+/**
+ * Created by user on 12/02/16.
+ */
+public class Mutagen {
+
+
+
+}
--- MutationGenerator/src/test/java/spoon/test/generics/testclasses/Mole.java
+++
@@ -1,9 +0,0 @@
-package spoon.test.generics.testclasses;
-
-public class Mole {
-	public void cook() {
-		class Cook<T> {
-		}
-		final Cook<String> aClass = new Cook<String>();
-	}
-}
