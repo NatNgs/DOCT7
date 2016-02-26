a58e22b5bfc7ac693678fe1b54c2b25eb1c68ff9
Adrien Boucher <adspartan04@gmail.com>
2016-02-26 09:12:12 +0100

    Correction to exec.sh

--- exec.sh
+++ exec.sh
@@ -16,21 +16,21 @@ echo ".Done"
 
 cd target
 
-jar=find -name "mutationGenerator*.jar"
+jar=$(find -name "mutationGenerator*.jar")
 
 java -jar $jar ../../OriginalSrc ../../MutatedSrc
 
 cd ../..
 
-fichiers=`ls ./MutadedSrc/`
+fichiers=`ls ./MutatedSrc/`
 i=1;
 for fichier in $fichiers
 do
 	echo -n "Build all $fichier .."
-	cd "./MutadedSrc/$fichier/"
+	cd "./MutatedSrc/$fichier/"
 	# bash build_all.sh &> /dev/null 
 	cd ../..
-	cp -Rf "./MutadedSrc/$fichier/j2e/target/surefire-reports/" ./TempResult/
+	cp -Rf "./MutatedSrc/$fichier/j2e/target/surefire-reports/" ./TempResult/
 	mkdir ./TempResult/surefire-reports-$i/
 	mv -f ./TempResult/surefire-reports/* ./TempResult/surefire-reports-$i/
 	i=$((i + 1))
@@ -47,4 +47,5 @@ cd "./XmlsCompiler/out/artifacts/XmlsCompiler_jar/"
 java -jar XmlsCompiler.jar "$dossier"
 
 mv Result.html ../../../../
+
 
