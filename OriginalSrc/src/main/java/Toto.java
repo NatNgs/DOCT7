961ea1b01e1d5e87fb913cb44cdcbec95e9ce864
Adrien Boucher <adspartan04@gmail.com>
2016-03-04 08:28:48 +0100

    Ajout d'un projet type pour tester spoon et la chaine de build complete

---
+++ OriginalSrc/src/main/java/Toto.java
@@ -0,0 +1,31 @@
+/**
+ * Created by user on 04/03/16.
+ */
+public class Toto {
+    public Toto(){}
+
+    public int ultimateAnwser()
+    {
+        int anwser = 42;
+
+        return anwser;
+    }
+
+    public int fact(int i)
+    {
+        if(i<0)
+            return -1;
+
+        int fact = 1;
+        for(int n=2; n<=i; n++)
+            fact *= n;
+        return fact;
+    }
+
+    public boolean isTrue(boolean b)
+    {
+        if(b == true)
+            return true;
+        return false;
+    }
+}
