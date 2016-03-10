fa16630b5aa69592608e8583e0d61724c907795b
Persenda <persenda@polytech.unice.fr>
2016-03-10 23:29:07 +0100

    [add]

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo -n "Install mutation generator..."
 cd MutationGenerator
-mvn clean install > "$racine/Reports/Mutagen mvn install.txt"
+mvn clean install >> "../Reports/Mutagen mvn install.txt"
 cd ../MutationApply
 bash build.sh
 cd ..
