80bcb678a29943c9a3d89bea24087fe4a065de83
Adrien Boucher <adspartan04@gmail.com>
2016-03-10 09:20:20 +0100

    MutationApply base project

---
+++ MutationApply/src/main/java/fr/unice/polytech/devops/groupe7/mutation/Main.java
@@ -0,0 +1,19 @@
+package fr.unice.polytech.devops.groupe7.mutation;
+
+/**
+ * Created by user on 10/03/16.
+ */
+public class Main {
+
+    public static void Main(String args[])
+    {
+        if(args.length != 2)
+        {
+            System.err.println("Incorrect arguments : <project_path> <mutator>");
+            return;
+        }
+
+        MutationApply ma = new MutationApply(args[0], args[1]);
+        ma.Apply();
+    }
+}
---
+++ MutationApply/src/main/java/fr/unice/polytech/devops/groupe7/mutation/MutationApply.java
@@ -0,0 +1,20 @@
+package fr.unice.polytech.devops.groupe7.mutation;
+
+/**
+ * Created by user on 10/03/16.
+ */
+public class MutationApply {
+    private String project;
+    private String mutator;
+
+    public MutationApply(String project, String mutator)
+    {
+        this.project = project;
+        this.mutator = mutator;
+    }
+
+    public void Apply()
+    {
+
+    }
+}
